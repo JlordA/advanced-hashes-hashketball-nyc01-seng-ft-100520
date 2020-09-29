cd8746dae13d8f360832093173d6527616221ff1
placid-tier-4550 <james.ardery@gmail.com>
2020-09-29 18:44:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,4 +148,20 @@ def num_points_scored(player_name)
 end
 
 def shoe_size(player_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |team_stat, stat_data|
+     if stat_data.class == Array
+        stat_data.each do |inner_team_stat|
+          if inner_team_stat.class == Hash
+            inner_team_stat.detect do |stat_key, stat_value|
+              if inner_team_stat[:player_name] == player_name
+                 return inner_team_stat[:shoe]
+               #binding.pry
+              end
+            end
+          end
+       end
+      end
+    end
+  end
 end
